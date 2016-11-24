0d80b67993d83819c41f13b748ce8a6dde4d7924
Lucas Schoenardie <lberteh@gmail.com>
2016-11-24 15:01:06 -0500

    Select Form created

---
+++ Assignment4/SelectForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Assignment4
+{
+    partial class SelectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHardwareList = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblYourSelection = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHardwareList
+            // 
+            this.lblHardwareList.AutoSize = true;
+            this.lblHardwareList.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHardwareList.Location = new System.Drawing.Point(22, 19);
+            this.lblHardwareList.Name = "lblHardwareList";
+            this.lblHardwareList.Size = new System.Drawing.Size(298, 22);
+            this.lblHardwareList.TabIndex = 0;
+            this.lblHardwareList.Text = "Dollar Computer Hardware List";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1011, 477);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // lblYourSelection
+            // 
+            this.lblYourSelection.AutoSize = true;
+            this.lblYourSelection.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYourSelection.Location = new System.Drawing.Point(22, 544);
+            this.lblYourSelection.Name = "lblYourSelection";
+            this.lblYourSelection.Size = new System.Drawing.Size(144, 22);
+            this.lblYourSelection.TabIndex = 2;
+            this.lblYourSelection.Text = "Your Selection";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(182, 541);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(582, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(806, 541);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(111, 30);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(926, 541);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(111, 30);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // SelectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 593);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblYourSelection);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblHardwareList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "SelectForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Select Your Computer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHardwareList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblYourSelection;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
