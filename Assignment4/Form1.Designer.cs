708fc89b29a61455cc9efa56e6a9666fc29d8459
Lucas Schoenardie <lberteh@gmail.com>
2016-11-24 14:53:52 -0500

    Start Form created

--- Assignment4/Form1.Designer.cs
+++ Assignment4/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Assignment4
 {
-    partial class Form1
+    partial class StartForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.lblOrderYourCompuer = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNewOrder = new System.Windows.Forms.Button();
+            this.btnOpenSaved = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblOrderYourCompuer
+            // 
+            this.lblOrderYourCompuer.AutoSize = true;
+            this.lblOrderYourCompuer.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderYourCompuer.Location = new System.Drawing.Point(32, 31);
+            this.lblOrderYourCompuer.Name = "lblOrderYourCompuer";
+            this.lblOrderYourCompuer.Size = new System.Drawing.Size(273, 22);
+            this.lblOrderYourCompuer.TabIndex = 0;
+            this.lblOrderYourCompuer.Text = "Order your computer today!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(64, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNewOrder
+            // 
+            this.btnNewOrder.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewOrder.Location = new System.Drawing.Point(64, 289);
+            this.btnNewOrder.Name = "btnNewOrder";
+            this.btnNewOrder.Size = new System.Drawing.Size(204, 33);
+            this.btnNewOrder.TabIndex = 2;
+            this.btnNewOrder.Text = "Start New Order";
+            this.btnNewOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenSaved
+            // 
+            this.btnOpenSaved.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenSaved.Location = new System.Drawing.Point(64, 328);
+            this.btnOpenSaved.Name = "btnOpenSaved";
+            this.btnOpenSaved.Size = new System.Drawing.Size(204, 33);
+            this.btnOpenSaved.TabIndex = 3;
+            this.btnOpenSaved.Text = "Open a Saved Order";
+            this.btnOpenSaved.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(64, 367);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(204, 33);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(332, 434);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOpenSaved);
+            this.Controls.Add(this.btnNewOrder);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblOrderYourCompuer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome to Dollar Computer!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblOrderYourCompuer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNewOrder;
+        private System.Windows.Forms.Button btnOpenSaved;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
